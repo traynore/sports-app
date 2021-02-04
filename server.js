0b2ee50d492781e0a8571bd190eb848b93d2df6d
traynore <Emmett.traynor@gmail.com>
2021-02-04 17:24:48 +0500

    first commit

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ if (process.env.NODE_ENV === "production") {
 
 
 const PORT = process.env.PORT || 5000;
-app.set('port', process.env.PORT || 5000);
+//app.set('port', process.env.PORT || 5000);
 
 app.listen(PORT, () => console.log(`Server running on PORT localhost:${PORT}`));
